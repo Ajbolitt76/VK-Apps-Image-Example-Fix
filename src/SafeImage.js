09467899c55d06c854c72bbb1316edb80a130f9d
Ajbolit76 <78945qw@mail.ru>
2020-09-14 22:51:46 +0500

    Fix: variant 1

--- src/SafeImage.js
+++ src/SafeImage.js
@@ -2,6 +2,6 @@ import React, {useState, useEffect} from 'react';
 
 export default function SafeImage(props) {
     return(
-        <img src={ props.src } alt={ props.alt } referrerPolicy="no-referrer"/>
+        <img src={ props.src } alt={ props.alt } referrerPolicy="strict-origin"/>
     )
 }
